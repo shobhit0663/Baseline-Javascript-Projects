43cebea028df2341e833a45c72df2de3f2075a77
Shobhit Gaidhane <155177835+shobhit0663@users.noreply.github.com>
2025-02-15 01:35:10 +0530

    Delete 84-Text Resize directory

--- 84-Text Resize/app.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const fontSizeInput = document.getElementById("fontSize");
-    const textToResize = document.getElementById("textToResize");
-
-    fontSizeInput.addEventListener("input", function () {
-        const newSize = fontSizeInput.value + "px";
-        textToResize.style.fontSize = newSize;
-    });
-});
