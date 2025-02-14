a2dac8e398cd53ac08ed5dada8bd2893886ee0ff
Shobhit Gaidhane <155177835+shobhit0663@users.noreply.github.com>
2025-02-15 01:33:42 +0530

    Delete 89-Aria Role Implementation directory

--- 89-Aria Role Implementation/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const alertBtn = document.getElementById("alertBtn");
-
-    alertBtn.addEventListener("click", function () {
-        alert("Hello, this is an alert!");
-    });
-});
